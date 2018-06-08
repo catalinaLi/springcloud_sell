6d37d6a2df09fa0824fae39949d00e11701ebc9a
lixin <lixin@weyao.com>
2018-06-08 17:29:06 +0800

    complete order

--- order/order-server/src/main/java/top/catalinali/order/server/OrderApplication.java
+++ order/order-server/src/main/java/top/catalinali/order/server/OrderApplication.java
@@ -3,9 +3,11 @@ package top.catalinali.order.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "top.catalinali.product.client")
 public class OrderApplication {
 
     public static void main(String[] args) {
--- order/order-server/src/main/java/top/catalinali/order/server/service/impl/OrderServiceImpl.java
+++ order/order-server/src/main/java/top/catalinali/order/server/service/impl/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package top.catalinali.order.server.service.impl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import top.catalinali.order.server.dataobject.OrderDetail;
 import top.catalinali.order.server.dataobject.OrderMaster;
 import top.catalinali.order.server.dto.OrderDto;
 import top.catalinali.order.server.enums.OrderStatusEnum;
@@ -11,8 +12,14 @@ import top.catalinali.order.server.repository.OrderDetailRepository;
 import top.catalinali.order.server.repository.OrderMasterRepository;
 import top.catalinali.order.server.service.OrderService;
 import top.catalinali.order.server.utils.KeyUtil;
+import top.catalinali.product.client.ProductClient;
+import top.catalinali.product.common.DecreaseStockInput;
+import top.catalinali.product.common.ProductInfoOutput;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Author: lx
@@ -29,21 +36,44 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMasterRepository orderMasterRepository;
 
+    @Autowired
+    private ProductClient productClient;
+
     @Override
     public OrderDto create(OrderDto orderDto) {
         String orderId = KeyUtil.genUniqueKey();
 
-        //todo 查询商品信息(调用商品服务)
+        //查询商品信息(调用商品服务)
+        List<String> productIdList = orderDto.getOrderDetailList().stream().map(OrderDetail::getOrderId).collect(Collectors.toList());
+        List<ProductInfoOutput> productInfoList = productClient.listForOrder(productIdList);
+        //计算总价
+        BigDecimal orderAmout = new BigDecimal(BigInteger.ZERO);
+        for (OrderDetail orderDetail: orderDto.getOrderDetailList()) {
+            for (ProductInfoOutput productInfo: productInfoList) {
+                if (productInfo.getProductId().equals(orderDetail.getProductId())) {
+                    //单价*数量
+                    orderAmout = productInfo.getProductPrice()
+                            .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                            .add(orderAmout);
+                    BeanUtils.copyProperties(productInfo, orderDetail);
+                    orderDetail.setOrderId(orderId);
+                    orderDetail.setDetailId(KeyUtil.genUniqueKey());
+                    //订单详情入库
+                    orderDetailRepository.save(orderDetail);
+                }
+            }
+        }
 
-        //todo 计算总价
-
-        //todo 扣库存(调用商品服务)
-
-        //todo 订单入库
+        //扣库存(调用商品服务)
+        List<DecreaseStockInput> decreaseStockInputList = orderDto.getOrderDetailList().stream()
+                .map(e -> new DecreaseStockInput(e.getProductId(), e.getProductQuantity()))
+                .collect(Collectors.toList());
+        productClient.decreaseStock(decreaseStockInputList);
+        //订单入库
         OrderMaster orderMaster = new OrderMaster();
         orderDto.setOrderId(orderId);
         BeanUtils.copyProperties(orderDto, orderMaster);
-        orderMaster.setOrderAmount(new BigDecimal(5));
+        orderMaster.setOrderAmount(orderAmout);
         orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
         orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
         orderMasterRepository.save(orderMaster);
