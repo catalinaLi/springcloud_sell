6056dafbce26cdbb561b8a5798a272c176df83d7
lixin <lixin@weyao.com>
2018-07-06 16:27:21 +0800

    update api-gateway

---
+++ api-gateway/src/main/java/top/catalinali/apigateway/filter/AddResponseHeader.java
@@ -0,0 +1,46 @@
+package top.catalinali.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.UUID;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+
+/**
+ * @Author: lllx
+ * @Description: 后过滤器
+ * @Date: Created on 16:19 2018/7/6
+ * @Modefied by:
+ */
+@Component
+public class AddResponseHeader extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletResponse response = context.getResponse();
+        response.setHeader("X-Foo", UUID.randomUUID().toString());
+        return null;
+    }
+}
