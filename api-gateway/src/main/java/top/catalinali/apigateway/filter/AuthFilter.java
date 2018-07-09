28c0c1112a34bfe927f95c4ad39639256b4057ab
lixin <lixin@weyao.com>
2018-07-09 11:54:05 +0800

    start AuthFilter

---
+++ api-gateway/src/main/java/top/catalinali/apigateway/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package top.catalinali.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * @Author: lllx
+ * @Description: token拦截
+ * @Date: Created on 15:34 2018/7/6
+ * @Modefied by:
+ */
+@Component
+public class AuthFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER -1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        return null;
+    }
+}
