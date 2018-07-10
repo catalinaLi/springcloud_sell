6a9c644d9e85a1a2e3470d4750a59a2cbf84e87f
lixin <lixin@weyao.com>
2018-07-10 16:52:30 +0800

    complete filter

---
+++ api-gateway/src/main/java/top/catalinali/apigateway/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package top.catalinali.apigateway.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+/**
+ * @Author: lllx
+ * @Description: 跨域配置
+ * @Date: Created on 16:49 2018/7/10
+ * @Modefied by:
+ */
+@Configuration
+public class CorsConfig {
+    //FIXME 新版spring boot有问题
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("*"));// http:www.a.com
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setMaxAge(300l);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter();
+    }
+
+}
---
+++ api-gateway/src/main/java/top/catalinali/apigateway/config/ZuulConfig.java
@@ -0,0 +1,22 @@
+package top.catalinali.apigateway.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: lllx
+ * @Description:
+ * @Date: Created on 16:49 2018/7/10
+ * @Modefied by:
+ */
+@Component
+public class ZuulConfig {
+
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+}
